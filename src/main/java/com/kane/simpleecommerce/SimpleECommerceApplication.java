2a97b3223538f5863e2165de60480fd094e6ea12
Cuong Le <le.van.cuong+1@gridsolutions.co.jp>
2024-05-02 02:28:25 +0900

    1st commit

---
+++ src/main/java/com/kane/simpleecommerce/SimpleECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.kane.simpleecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kane/simpleecommerce/SimpleECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kane.simpleecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
