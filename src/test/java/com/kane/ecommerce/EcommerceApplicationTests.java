ab272471e3c067ea44a35c5e898a05a22f17dd7e
Cuong Le <le.van.cuong+1@gridsolutions.co.jp>
2024-05-03 02:31:23 +0900

    fix: rename SimpleECommerceApplication to EcommerceApplication and related works

--- src/test/java/com/kane/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/kane/ecommerce/EcommerceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.kane.simpleecommerce;
+package com.kane.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SimpleECommerceApplicationTests {
+class EcommerceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
